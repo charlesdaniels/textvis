6020ddeecbeb896ec2d3859132072002d8e09090
Charles Daniels <cdaniels@fastmail.com>
2019-04-07 15:54:17 -0400

    play with some sample data

--- src/graph.js
+++ src/graph.js
@@ -1,32 +1,51 @@
+// var nodes = [
+//   { id: "mammal", group: 0, label: "Mammals", level: 1 },
+//   { id: "dog"   , group: 0, label: "Dogs"   , level: 2 },
+//   { id: "cat"   , group: 0, label: "Cats"   , level: 2 },
+//   { id: "fox"   , group: 0, label: "Foxes"  , level: 2 },
+//   { id: "elk"   , group: 0, label: "Elk"    , level: 2 },
+//   { id: "insect", group: 1, label: "Insects", level: 1 },
+//   { id: "ant"   , group: 1, label: "Ants"   , level: 2 },
+//   { id: "bee"   , group: 1, label: "Bees"   , level: 2 },
+//   { id: "fish"  , group: 2, label: "Fish"   , level: 1 },
+//   { id: "carp"  , group: 2, label: "Carp"   , level: 2 },
+//   { id: "pike"  , group: 2, label: "Pikes"  , level: 2 }
+// ]
+// 
+// var links = [
+//     { target: "mammal", source: "dog" , strength: 0.7 },
+//     { target: "mammal", source: "cat" , strength: 0.7 },
+//   { target: "mammal", source: "fox" , strength: 0.7 },
+//   { target: "mammal", source: "elk" , strength: 0.7 },
+//   { target: "insect", source: "ant" , strength: 0.7 },
+//   { target: "insect", source: "bee" , strength: 0.7 },
+//   { target: "fish"  , source: "carp", strength: 0.7 },
+//   { target: "fish"  , source: "pike", strength: 0.7 },
+//   { target: "cat"   , source: "elk" , strength: 0.1 },
+//   { target: "carp"  , source: "ant" , strength: 0.1 },
+//   { target: "elk"   , source: "bee" , strength: 0.1 },
+//   { target: "dog"   , source: "cat" , strength: 0.1 },
+//   { target: "fox"   , source: "ant" , strength: 0.1 },
+//     { target: "pike"  , source: "cat" , strength: 0.1 }
+// ]
+
 var nodes = [
-  { id: "mammal", group: 0, label: "Mammals", level: 1 },
-  { id: "dog"   , group: 0, label: "Dogs"   , level: 2 },
-  { id: "cat"   , group: 0, label: "Cats"   , level: 2 },
-  { id: "fox"   , group: 0, label: "Foxes"  , level: 2 },
-  { id: "elk"   , group: 0, label: "Elk"    , level: 2 },
-  { id: "insect", group: 1, label: "Insects", level: 1 },
-  { id: "ant"   , group: 1, label: "Ants"   , level: 2 },
-  { id: "bee"   , group: 1, label: "Bees"   , level: 2 },
-  { id: "fish"  , group: 2, label: "Fish"   , level: 1 },
-  { id: "carp"  , group: 2, label: "Carp"   , level: 2 },
-  { id: "pike"  , group: 2, label: "Pikes"  , level: 2 }
+    { id: "001", label: "Paragraph 1", level: 1 },
+    { id: "002", label: "This is the first sentence.", level: 2 },
+    { id: "003", label: "This is the second sentence.", level: 2 },
+    { id: "004", label: "This is the third sentence.", level: 2 },
+    { id: "005", label: "Paragraph 2", level: 1},
+    { id: "006", label: "Look another sentence!", level: 2},
 ]
 
 var links = [
-	{ target: "mammal", source: "dog" , strength: 0.7 },
-	{ target: "mammal", source: "cat" , strength: 0.7 },
-  { target: "mammal", source: "fox" , strength: 0.7 },
-  { target: "mammal", source: "elk" , strength: 0.7 },
-  { target: "insect", source: "ant" , strength: 0.7 },
-  { target: "insect", source: "bee" , strength: 0.7 },
-  { target: "fish"  , source: "carp", strength: 0.7 },
-  { target: "fish"  , source: "pike", strength: 0.7 },
-  { target: "cat"   , source: "elk" , strength: 0.1 },
-  { target: "carp"  , source: "ant" , strength: 0.1 },
-  { target: "elk"   , source: "bee" , strength: 0.1 },
-  { target: "dog"   , source: "cat" , strength: 0.1 },
-  { target: "fox"   , source: "ant" , strength: 0.1 },
-	{ target: "pike"  , source: "cat" , strength: 0.1 }
+    { target: "001", source: "002", strength: 0.5 },
+    { target: "001", source: "003", strength: 0.5 },
+    { target: "001", source: "004", strength: 0.5 },
+    { target: "002", source: "003", strength: 0.5 },
+    { target: "003", source: "004", strength: 0.5 },
+    { target: "001", source: "005", strength: 0.1 },
+    { target: "005", source: "006", strength: 0.5 },
 ]
 
 function getNeighbors(node) {
